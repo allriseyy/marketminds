6c820722e8e70e5997378281bec0e3f61c496789
allriseyy <allriseyy@gmail.com>
2025-05-19 15:16:14 +0100

    2-add stock and comment models

---
+++ api/Models/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Comment
+{
+	public Comment()
+	{
+        public int Id { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+		public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+		public int? StockId { get; set; }
+		public Stock? Stock { get; set; }
+	}
+}

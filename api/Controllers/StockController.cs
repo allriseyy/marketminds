8a49bf6b5d7413b88aaeab496e4c0b62d7dea2f9
allriseyy <allriseyy@gmail.com>
2025-05-28 18:27:33 +0100

    8-delete (#9)

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -60,5 +60,18 @@ namespace api.Controllers
 
             return Ok(stockModel.toStockDto());
         }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            var stockModel = _context.Stocks.FirstOrDefault(x => x.Id == id);
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+            _context.Stocks.Remove(stockModel);
+            _context.SaveChanges();
+            return NoContent();
+        }
     }
 }
